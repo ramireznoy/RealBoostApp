f594126aab1d58bd75fbc301bc9423182d1996da
Luis Enrique <ramireznoy@gmail.com>
2017-05-16 20:49:25 -0700

    External user list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 
@@ -15,6 +16,7 @@ import { ContactsProvider } from '../providers/contacts/contacts';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp, {mode: 'ios' /* use 'md' 'ios' or 'wp' to force theming */ })
   ],
   bootstrap: [IonicApp],
--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 
+import { ContactsProvider } from '../../providers/contacts/contacts';
+
 /**
  * Generated class for the ContactsPage page.
  *
@@ -12,20 +14,30 @@ import { Events } from 'ionic-angular';
 @Component({
   selector: 'page-contacts',
   templateUrl: 'contacts.html',
+  providers: [ContactsProvider]
 })
 export class ContactsPage {
   name: string;
   username: string;
   email: string;
+  contacts: any;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events, public contactsProvider: ContactsProvider) {
     this.name = localStorage.getItem('name');
     this.username = localStorage.getItem('username');
     this.email = localStorage.getItem('email');
+    this.loadContacts();
   }
 
   ionViewDidEnter() {
-    this.events.publish('tab:selected', 'Contacts');
+    this.events.publish('tab:selected', 'Contacts');    
+  }
+
+  loadContacts(){
+    this.contactsProvider.load()
+    .then(data => {
+      this.contacts = data;
+    });
   }
   
 }
